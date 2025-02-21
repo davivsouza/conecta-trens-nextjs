a315e3c4a1b72921160b859b63a87b0c177efd7c
davivsouza <davivasconcelssouza7@gmail.com>
2025-02-21 20:03:31 -0300

    feat: change favico

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,8 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "ConectaTrens",
-  description: "ConectaTrens é uma solução que visa melhorar a acessibilidade digital dos sites das linhas de trem operadas pela CCR, unificando e otimizando a experiência dos usuários. Nosso foco é facilitar o acesso às informações essenciais e gerar declarações de atraso de maneira rápida e intuitiva para públicos com menor familiaridade tecnológica",
+  description:
+    "ConectaTrens é uma solução que visa melhorar a acessibilidade digital dos sites das linhas de trem operadas pela CCR, unificando e otimizando a experiência dos usuários. Nosso foco é facilitar o acesso às informações essenciais e gerar declarações de atraso de maneira rápida e intuitiva para públicos com menor familiaridade tecnológica",
 };
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <link rel="icon" href="/logo2.svg" sizes="any" />
       <body className={`${inter.className} antialiased`}>
         {children}
         <VLibrasComponent />
